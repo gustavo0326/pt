02ac356e29de4b3644221999537f9da91cd537a2
gustavo0326 <gustavo0326@gmail.com>
2016-02-13 21:42:48 -0500

    probando

--- app/src/main/java/com/example/julianandres/parquedelcafe/ListaAtracciones.java
+++ app/src/main/java/com/example/julianandres/parquedelcafe/ListaAtracciones.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ListaAtracciones extends AppCompatActivity {
-
+//cambio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
